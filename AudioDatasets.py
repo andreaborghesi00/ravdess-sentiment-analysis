ce0586e6c2644c913a35f38fb8a83780af7e7a22
coso <00borghesiandrea@gmail.com>
2024-07-08 14:51:45 +0200

    Initial commit

--- AudioDatasets.py
+++ AudioDatasets.py
@@ -84,31 +84,26 @@ def pitch(data, sampling_rate, pitch_factor=0.7):
 
 
 def extract_features(data, sample_rate):
-    # ZCR
     result = np.array([])
     zcr = np.mean(librosa.feature.zero_crossing_rate(y=data).T, axis=0)
-    result=np.hstack((result, zcr)) # stacking horizontally
+    result=np.hstack((result, zcr))
 
-    # Chroma_stft
     stft = np.abs(librosa.stft(data))
     chroma_stft = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
-    result = np.hstack((result, chroma_stft)) # stacking horizontally
+    result = np.hstack((result, chroma_stft))
 
-    # MFCC
     mfcc = np.mean(librosa.feature.mfcc(y=data, sr=sample_rate).T, axis=0)
-    result = np.hstack((result, mfcc)) # stacking horizontally
+    result = np.hstack((result, mfcc))
 
-    # Root Mean Square Value
     rms = np.mean(librosa.feature.rms(y=data).T, axis=0)
-    result = np.hstack((result, rms)) # stacking horizontally
+    result = np.hstack((result, rms))
 
-    # MelSpectogram
     mel = np.mean(librosa.feature.melspectrogram(y=data, sr=sample_rate).T, axis=0)
-    result = np.hstack((result, mel)) # stacking horizontally
-    
+    result = np.hstack((result, mel))
+
     return result
 
-def get_features(path, augment=False):
+def get_features(path, transforms=None):
     # duration and offset are used to take care of the no audio in start and the ending of each audio files as seen above.
     data, sample_rate = librosa.load(path, duration=2.5, offset=0.6)
     
@@ -116,7 +111,7 @@ def get_features(path, augment=False):
     res1 = extract_features(data, sample_rate)
     result = np.array(res1)
     
-    if augment:
+    if transforms is not None:
         # data with noise
         noise_data = noise(data, sample_rate)
         res2 = extract_features(noise_data, sample_rate)
@@ -130,34 +125,5 @@ def get_features(path, augment=False):
     
     return result
 
-
-def augment_data(X, y, augmentation_prob=0.5, transform=None, dataset_multiplier=2, save_path=None, force_recompute=False, normalizer=None, train=False):
-    global precomputed_dir
-    X_features = []
-
-    if not force_recompute and os.path.exists(os.path.join(precomputed_dir, save_path)):
-        print(f"Loading precomputed data from {save_path}")
-        X_features = np.load(os.path.join(precomputed_dir, save_path))
-        y = np.array(y)
-        y = np.repeat(y, dataset_multiplier, axis=0)
-        return X_features, y, normalizer
-    
-    print("Computing features...")
-    for m in range(dataset_multiplier):
-        print(f"Augmentation {m+1}/{dataset_multiplier}")
-        for idx in tqdm(range(len(X))):
-            feats = get_features(X[idx], augmentation_prob=augmentation_prob, transform=transform)
-            X_features.append(feats)
-    X_features = np.array(X_features)
-    if train and normalizer is not None:
-        X_features = normalizer.fit_transform(X_features)
-    elif normalizer is not None:
-        X_features = normalizer.transform(X_features)
-
-    y = np.array(y)
-    print(f"Saving precomputed data at {os.path.join(precomputed_dir, save_path)}")
-    if save_path != None:
-        np.save(os.path.join(precomputed_dir, save_path), X_features)
-    return X_features, y, normalizer
 
         
